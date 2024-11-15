01276e85005148b09797eb6359d2bfb15887c6ba
Melissa Knutzen <129540349+oll-melissaknutzen@users.noreply.github.com>
2024-11-15 17:27:31 -0600

    gg

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -42,7 +42,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutritional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        ssf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
 
