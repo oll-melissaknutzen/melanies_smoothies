b60189d55209516ecca8563119946e89126d5820
Melissa Knutzen <129540349+oll-melissaknutzen@users.noreply.github.com>
2024-11-15 12:19:26 -0600

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.write(smoothiefroot_response)
+st.json(smoothiefroot_response)
